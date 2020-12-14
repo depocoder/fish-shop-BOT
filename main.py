ef1db8bb1983a123a54ed31a4073b47aca7387b4
herypank <60302361+herypank@users.noreply.github.com>
2020-12-14 17:22:31 +0300

    add get_id_item

--- main.py
+++ main.py
@@ -11,3 +11,11 @@ def get_access_token():
         'grant_type': 'implicit'
     })
     return json.loads(response.text)['access_token']
+
+
+def get_id_item(access_token):
+    response = requests.get('https://api.moltin.com/v2/products', headers={
+        'Authorization': f'Bearer {access_token}',
+        'Content-Type': 'application/json',
+    })
+    return json.loads(response.text)['data'][0]['id']
