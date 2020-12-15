c51666471978816bb2b568f106db3bd3b5c8bac2
herypank <60302361+herypank@users.noreply.github.com>
2020-12-15 11:26:10 +0300

    Ренейминг

--- motlin_api.py
+++ motlin_api.py
@@ -13,12 +13,12 @@ def get_access_token():
     return json.loads(response.text)['access_token']
 
 
-def get_id_item(access_token):
+def get_items(access_token):
     response = requests.get('https://api.moltin.com/v2/products', headers={
         'Authorization': f'Bearer {access_token}',
         'Content-Type': 'application/json',
     })
-    return json.loads(response.text)['data'][0]['id']
+    return json.loads(response.text)['data']
 
 
 def add_to_cart(access_token):
@@ -26,8 +26,7 @@ def add_to_cart(access_token):
         'Authorization': 'Bearer XXXX',
         'Content-Type': 'application/json',
         }
-    item_id = get_id_item(access_token)
-    print(item_id)
+    item_id = get_items(access_token)[0]['id']
     data = {"data": {"id": item_id,
                      "type": "cart_item", "quantity": 1}}
     headers = {
@@ -38,7 +37,6 @@ def add_to_cart(access_token):
     response = requests.post(
         'https://api.moltin.com/v2/carts/:reference/items', headers=headers,
         data=json.dumps(data))
-    print(response.text)
 
 
 if __name__ == "__main__":
